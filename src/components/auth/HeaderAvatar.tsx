aa9bbe5d4c11e561bcfdf33bbbd79361c4a7db49
Cristian Perez <87153751+PerezCristian-dev@users.noreply.github.com>
2023-10-07 01:34:20 -0400

    Update: Header Avatar Title hidden on mobile view.

--- src/components/auth/HeaderAvatar.tsx
+++ src/components/auth/HeaderAvatar.tsx
@@ -8,14 +8,27 @@ import { useSession } from "next-auth/react";
 
 export const HeaderAvatar = () => {
   const { data, status } = useSession();
+  const [isMobile, setIsMobile] = React.useState(false);
   const handleLogout = () => {
     signOut();
   };
 
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth > 600);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     status === "authenticated" && (
       <div className="flex items-center relative right-0 z-50">
-        <span className="mr-2">{data?.user?.name}</span>
+        {isMobile && <span className="mr-2">{data?.user?.name}</span>}
         <DropDownMenu icon={data?.user?.image || ""} isImage>
           <li>
             <a onClick={handleLogout}>
